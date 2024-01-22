e041a360e16321ce63885c66628322f01caa001d
Vikas Gautam <gautamvikas754@gmail.com>
2024-01-22 16:47:20 +0530

    willpopscope replaced with popscope

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -54,15 +54,16 @@ class _FiltersScreenState extends State<FiltersScreen> {
       //     }
       //   },
       // ),
-      body: WillPopScope(
-        onWillPop: () async {
+      body: PopScope(
+        canPop: false,
+        onPopInvoked: (bool didPop) {
+          if (didPop) return;
           Navigator.of(context).pop({
             Filter.glutenFree: _glutenFreeFilterSet,
             Filter.lactoseFree: _lactoseFreeFilterSet,
             Filter.vegetarian: _vegetarianFilterSet,
             Filter.vegan: _veganFilterSet,
           });
-          return false;
         },
         child: Column(
           children: [
